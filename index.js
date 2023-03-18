2513bf505c4f80e15af58be212c36890759dcbfa
Guled <guletkadar@gmail.com>
2023-03-18 22:26:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+
+// Task (1)
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log("this is the result of Task (1) =  " +multiply);
+
+// Task (2)
+
+let random = Math.floor(Math.random() * 5) + 2;
+
+console.log("this is the result of Task (2) " + random);
+
+// Task (3)
+
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+console.log("The reminder of this two numbers is " +mod);
+
+// Task (4)
+
+let max = Math.max(5,10,15,20);
+console.log("The highest num of this values is " +max);
+
+
+
